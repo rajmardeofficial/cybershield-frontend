149c8a2c0aca054e686de6778047321ecbcb36c6
tejas <tejassable07@gmail.com>
2024-04-12 10:49:38 +0530

    firstScreenSecondScreen

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import NavBarComp from './Components/FirstScreen/NavBarComp/NavBarComp';
-import GreenComp from './Components/FirstScreen/GreenComp/GreenComp';
-import GreyComp from './Components/FirstScreen/GreyComp/GreyComp';
-import BlueComp from './Components/FirstScreen/BlueComp/BlueComp';
-import Cara from './Components/FirstScreen/Carousel/Cara';
-import ChatWAdvct from './Components/FirstScreen/SecondScreen/Call';
-function App() {
-  return (
-    <>
+// import React from 'react';
+// import NavBarComp from './Components/FirstScreen/NavBarComp/NavBarComp';
+// import GreenComp from './Components/FirstScreen/GreenComp/GreenComp';
+// import GreyComp from './Components/FirstScreen/GreyComp/GreyComp';
+// import BlueComp from './Components/FirstScreen/BlueComp/BlueComp';
+// import Cara from './Components/FirstScreen/Carousel/Cara';
+// import ChatWAdvct from './Components/FirstScreen/SecondScreen/Call';
+// function App() {
+//   return (
+//     <>
       
-        <NavBarComp />
+//         <NavBarComp />
       
-      <div style={{ marginTop:"1.5%", marginBottom: '20px' }}>
-        <GreenComp />
-      </div>
-      <div style={{ marginBottom: '20px', width: '90%', marginLeft: '5%' }}>
-        <GreyComp />
-      </div>
-      <div style={{ marginBottom: '20px' }}>
-        <BlueComp />
-      </div>
-      <Cara />
-      {/* <ChatWAdvct/> */}
-    </>
-  );
-}
+//       <div style={{ marginTop:"1.5%", marginBottom: '20px' }}>
+//         <GreenComp />
+//       </div>
+//       <div style={{ marginBottom: '20px', width: '90%', marginLeft: '5%' }}>
+//         <GreyComp />
+//       </div>
+//       <div style={{ marginBottom: '20px' }}>
+//         <BlueComp />
+//       </div>
+//       <Cara />
+//       {/* <ChatWAdvct/> */}
+//     </>
+//   );
+// }
 
-export default App;
+// export default App;
