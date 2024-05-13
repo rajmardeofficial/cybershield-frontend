344ea103e91885078a0b7b08569feb4782175f34
Siddharth Santosh Shinde <142812176+sid1514@users.noreply.github.com>
2024-05-13 17:32:41 +0530

    created signIn and sign up functionality

--- src/components/loginDetails/SignIn/signIn.js
+++ src/components/loginDetails/SignIn/signIn.js
@@ -1,40 +1,102 @@
-import './signin.css'
-import { useNavigate } from 'react-router-dom'
-const SignIN=()=>{
-  const nav=useNavigate()
-const  handleNavtoSignUp=()=>{
-  nav('/SignUp')
-}
-return(<>
+import { useState } from "react";
+import "./signin.css";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import {
+  Dimmer,
+  Loader,
+} from "semantic-ui-react";
+const SignIN = () => {
+  const nav = useNavigate();
+  const [email, setEmail] = useState();
+  const [password, setpassword] = useState();
+  const [loader, setLoader] = useState(false);
+  const [Error,setError]=useState(false)
+  const handleNavtoSignUp = () => {
+    nav("/SignUp");
+  };
+  const handleLogin = async () => {
+    if (!email && !password) {
+      setError(true)
+    }
+    try {
+      if (email && password) {
+        const res = await axios.post("http://localhost:8000/user/login", {
+          email,
+          password,
+        });
+        if (res) {
+          const token = res.data.result.token;
+          localStorage.setItem("token", token);
+          const userdata = res.data.result.userValid.fname;
+          localStorage.setItem("userId", userdata);
+          console.log(res);
+          setLoader(true);
+          nav("/");
+        } else if(res==null) {
+           setError(true);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      setError(true)
+    }
+  };
+  return (
+    <>
+      <div className=" d-flex  custom-size attached">
+        <div className=" custom-size3 ">
+          <h1>Sign in</h1>
+          <div>
+            <input
+              type="email"
+              className="input1"
+              aria-describedby="emailHelp"
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              aria-describedby="emailHelp"
+              placeholder="password"
+              className="input2"
+              onChange={(e) => setpassword(e.target.value)}
+            />
+            <label className="characters_pass">At least 8 characters</label>
+          </div>
+          {loader ? (
+            <Dimmer active>
+              <Loader size="massive">Loading</Loader>
+            </Dimmer>
+          ) : null}
+          <label className=" forgot_pass">Forgot your password</label>
+          <div className=" d-flex justify-content-center ">
+            {/* Modified here */}
+            <button className="signInbt" onClick={handleLogin}>
+              Sign in
+            </button>
+            <button className="signupbt" onClick={handleNavtoSignUp}>
+              Sign up
+            </button>
+          </div>
+        </div>
 
-
-<div className=' d-flex  custom-size'>
-  <div className=' custom-size3 '>
-    <h1 >Sign in</h1>
-    <div >
-      <input type="email" className="input1" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email" />
-      <input type="password" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="password" className='input2'/>
-      <label className='characters_pass'>At least 8 characters</label>     
-    </div>
-    <label id="" class=" forgot_pass">Forgot your password</label> 
-    <div className=' d-flex justify-content-center '> {/* Modified here */}
-      <button className='signInbt' >Sign in</button>
-      <button className="signupbt" onClick={handleNavtoSignUp}>Sign up</button>
-    </div>
-  </div>
-  
-  <div className=' custom-size2'>
-    <div className='  '>
-    <h1 className='text-hello'>Hello, Friend!</h1>
-    <p className='custom-text '>Enter your personal details and start your journey with us</p>
-    <button  onClick={handleNavtoSignUp}>Sign up</button>
-    </div>
-    
-  </div>
-</div>
-
-
-
-    </>)
-}
+        <div className=" custom-size2">
+          <div className="  ">
+            <h1 className="text-hello">Hello, Friend!</h1>
+            <p className="custom-text ">
+              Enter your personal details and start your journey with us
+            </p>
+            <button onClick={handleNavtoSignUp}>Sign up</button>
+          </div>
+        </div>
+      </div>
+      {Error?<div className="errorMessage">
+       
+        Incorrect password or email please try again
+      
+      </div>:null}
+    </>
+  );
+};
 export default SignIN;
--- src/components/loginDetails/SkillsDetails/skillsDetails.js
+++ src/components/loginDetails/SkillsDetails/skillsDetails.js
@@ -1,180 +1,339 @@
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useNavigate } from "react-router-dom";
+import "./skills.css";
+import axios from "axios";
 
-import { useState } from 'react';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
-import { useNavigate } from 'react-router-dom';
-import './skills.css'
-
-const SkillDetail=()=>{
-
-const [selectedImage, setSelectedImage] = useState(null);
-const nav=useNavigate()
-
-//for radio button
-const [selectedValue, setSelectedValue] = useState('');
+const SkillDetail = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [gender, setGender] = useState("");
+  const [location, setCity] = useState("");
+  const [marriedStatus, setMarriedStatus] = useState("");
+  const [specialization, setSpecialization] = useState("");
+  const [experienceYears, setExpYears] = useState("");
+  const [pincode, setPincode] = useState("");
+  const [workingOnOtherPlatforms, setSelectedRadioValue] = useState("");
+  const [source, setCyberShieldSource] = useState("");
+  const [primarySkill, setPrimarySkill] = useState("");
+  const [dailyHours, setContriHours] = useState("");
+  const [languages, setLanguage] = useState("");
+  const [formError, setFormError] = useState("");
+  const nav = useNavigate();
   const handleSelectionChange = (event) => {
-    setSelectedValue(event.target.value);
+    setSelectedRadioValue(event.target.value);
   };
 
-  //for upload image
-const handleImageChange = (event) => {
-  const file = event.target.files[0];
-    setSelectedImage(URL.createObjectURL(file));
-};
+  const [date, setDate] = useState("");
+  const [month, setMonth] = useState("");
+  const [year, setYear] = useState("");
 
-//for date of birth
-  const [selectedDate, setSelectedDate] = useState(null);
-  const [date, setDate] = useState('');
-  const [month, setMonth] = useState('');
-  const [year, setYear] = useState('');
-
-  const handleDateChange = date => {
+  const handleDateChange = (date) => {
     setSelectedDate(date);
-    const formattedDate = date ? date.getDate() : '';
-    const formattedYear = date ? date.getFullYear() : '';
-    const formattedMonth = date ? date.getMonth() : '';
+    const formattedDate = date ? date.getDate() : "";
+    const formattedYear = date ? date.getFullYear() : "";
+    const formattedMonth = date ? date.getMonth() : "";
     setYear(formattedYear);
     setDate(formattedDate);
-    setMonth(formattedMonth)
+    setMonth(formattedMonth);
+  };
+  //for upload image
+  const handleImageChange = (event) => {
+    setSelectedImage(event.target.files[0]);
   };
 
-  //next button event
-   const handleNavToOtherDetails=()=>{
-    nav('/othersDetails')
-   }
-    return(<>
-<section className='skills-form '>
+  const handleUpload = async () => {
+    const formData = new FormData();
+    formData.append("image", selectedImage);
+ const userId = localStorage.getItem("userid");
+    try {
+      await axios.post(
+        `http://localhost:8000/users/${userId}/upload`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      console.log("Image uploaded successfully");
+    } catch (error) {
+      console.error("Error uploading image: ", error);
+    }
+  };
+  const validateForm = () => {
+    // Validate form fields
+    if (
+      !gender ||
+      !location ||
+      !marriedStatus ||
+      !primarySkill ||
+      !languages ||
+      !experienceYears ||
+      !dailyHours ||
+      !source
+    ) {
+      setFormError("Please fill in all required fields.");
+      return false;
+    }
+    if (!selectedDate) {
+      setFormError("Please select a valid date of birth.");
+      return false;
+    }
+    if (!pincode) {
+      setFormError("Please enter a valid 6-digit pincode.");
+      return false;
+    }
 
-    {/*profile upload below*/}
-    <div className="container text-centre ">
-        <div className="row justify-content-md-center ">
-            
+    return true;
+  };
+
+  const sendDataToBackend = async () => {
+    try {
+      if (!validateForm) {
+        const userId = localStorage.getItem("userid");
+
+        const res = await axios.post(
+          `http://localhost:8000/users/${userId}/skills`,
+          {
+            gender,
+            dateOfBirth: selectedDate.toISOString(),
+            location,
+            pincode,
+            marriedStatus,
+            primarySkill,
+            specialization,
+            languages,
+            experienceYears,
+            dailyHours,
+            source,
+            workingOnOtherPlatforms,
+          }
+        );
+        if (res) {
+          nav("/othersDetails");
+        }
+      }
+      console.log("Data sent successfully!");
+    } catch (error) {
+      console.error("Error sending data:", error);
+    }
+  };
+
+  return (
+    <>
+      <section className="skills-form ">
+        {/*profile upload below*/}
+        <div className="container text-centre ">
+          <div className="row justify-content-md-center ">
             <div className="col col-lg-1 px-2 text-centre container img_section">
-                <span className=''>
-
-                <img src={selectedImage?selectedImage:"user.svg"} alt="your profile" className={selectedImage?"img-fluid rounded-circle bg-secondary-subtle mb-2 ":"img-fluid rounded-circle bg-secondary-subtle p-2  "} /> <br></br>
-                </span>
-                <label htmlFor="file-input" className="custom-file-upload bg-body fw-bold mt-2">Upload Photo</label>
-                <input type="file" id="file-input" name="ImageStyle" aria-label="upload" style={{display:'none'}} onChange={handleImageChange}/>  
-                                                            
+              <span className="">
+                <img
+                  src={selectedImage ? selectedImage : "user.svg"}
+                  alt="your profile"
+                  className={
+                    selectedImage
+                      ? "img-fluid rounded-circle bg-secondary-subtle mb-2 "
+                      : "img-fluid rounded-circle bg-secondary-subtle p-2  "
+                  }
+                />{" "}
+                <br></br>
+              </span>
+              <label
+                className="custom-file-upload bg-body fw-bold mt-2"
+                onClick={handleUpload}
+              >
+                Upload Photos
+              </label>
+              <input
+                type="file"
+                onChange={handleImageChange}
+              />
             </div>
-            <div className='row text-centre justify-content-md-center ms-5 mt-3 fw-bold'>
-                <div className='col col-lg-3 img_note'>
-                    Note-Image must be less than 5 MB
-                </div>
+            <label>{formError}</label>
+            <div className="row text-centre justify-content-md-center ms-5 mt-3 fw-bold">
+              <div className="col col-lg-3 img_note">
+                Note-Image must be less than 5 MB
+              </div>
             </div>
-            
+          </div>
         </div>
-        
-    </div>
-    {/*input form below*/}
-    <div className='container text-centre form1'>
-        <div className=' section1'>
+        {/*input form below*/}
+        <div className="container text-centre form1">
+          <div className=" section1">
             {/*gender input below*/}
-            <div className=' '>
-                <label>Gender</label>
-                <div class=" input_css">
-                    <select className="form-select h-100" id="inputGroupSelect01">
-                    <option></option>
-                    <option value="1">Male</option>
-                    <option value="2">Female</option>
-                    <option value="3">Other</option>
-                    </select>
-                </div>
+            <div className=" ">
+              <label>Gender</label>
+              <div className=" input_css">
+                <select
+                  className="form-select h-100"
+                  id="inputGroupSelect01"
+                  onChange={(e) => setGender(e.target.value)}
+                >
+                  <option></option>
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+                  <option value="Other">Other</option>
+                </select>
+              </div>
             </div>
             <div>
-                <label>Which city/state/country do you currently live in?</label> <br></br>
-                <input type='text' className='input_css'/>
+              <label>Which city/state/country do you currently live in?</label>{" "}
+              <br></br>
+              <input
+                type="text"
+                className="input_css"
+                onChange={(e) => setCity(e.target.value)}
+              />
             </div>
             <div>
-                <label>Married Status</label> <br></br>
-                <div class=" input_css">
-                    <select className="form-select h-100" id="inputGroupSelect01">
-                    <option></option>
-                    <option value="1">Married</option>
-                    <option value="2">Unmarried</option>
-                    <option value="3">Widow</option>
-                    </select>
-                </div>
+              <label>Married Status</label> <br></br>
+              <div className=" input_css">
+                <select
+                  className="form-select h-100"
+                  id="inputGroupSelect01"
+                  onChange={(e) => setMarriedStatus(e.target.value)}
+                >
+                  <option></option>
+                  <option value="Married">Married</option>
+                  <option value="Unmarried">Unmarried</option>
+                  <option value="Widowed">Widow</option>
+                  <option value="Divorced">Divorced</option>
+                </select>
+              </div>
             </div>
             <div>
-                <label>Add Specialization</label> <br></br>
-                <div class=" input_css">
-                    <select className="form-select h-100" id="inputGroupSelect01">
-                    <option></option>
-                    <option value="1">opt1</option>
-                    <option value="2">opt2</option>
-                    <option value="3">opt3</option>
-                    </select>
-                </div>
+              <label>Add Specialization</label> <br></br>
+              <div className=" input_css">
+                <select
+                  className="form-select h-100"
+                  id="inputGroupSelect01"
+                  onChange={(e) => setSpecialization(e.target.value)}
+                >
+                  <option></option>
+                  <option value="1">opt1</option>
+                  <option value="2">opt2</option>
+                  <option value="3">opt3</option>
+                </select>
+              </div>
             </div>
             <div>
-                <label>Experience in years</label> <br></br>
-                <input type='text' className='input_css'/>
+              <label>Experience in years</label> <br></br>
+              <input
+                type="text"
+                className="input_css"
+                onChange={(e) => setExpYears(e.target.value)}
+              />
             </div>
             <div>
-                <label>How many hours you can contribute daily?</label> <br></br>
-                <input type='text' className='input_css'/>
+              <label>where did you hear about cybershield india?</label>{" "}
+              <br></br>
+              <input
+                type="text"
+                className="input_css"
+                onChange={(e) => setCyberShieldSource(e.target.value)}
+              />
             </div>
-            
-           
-        </div> 
-        <div className=' section2'>
-            <div className=''>
-                <label>DOB</label> 
-                <div className='d-flex input-css date_input'>
-                  <label className=" h-100 date-add2" htmlFor="date-add"><img src="calendar.svg" className='img-date' alt="select date"/></label>
-                    <DatePicker selected={selectedDate} onChange={handleDateChange} dateFormat="dd" className=" date-add3" id="date-add" />
-                    <input value={month} className='date-add3' readOnly/>
-                    <input value={year} className='date-add4' readOnly/>
-                </div>
-           
+          </div>
+          <div className=" section2">
+            <div className="">
+              <label>DOB</label>
+              <div className="d-flex input-css date_input">
+                <label className=" h-100 date-add2" htmlFor="date-add">
+                  <img
+                    src="calendar.svg"
+                    className="img-date"
+                    alt="select date"
+                  />
+                </label>
+                <DatePicker
+                  selected={selectedDate}
+                  onChange={handleDateChange}
+                  dateFormat="dd"
+                  className=" date-add3"
+                  id="date-add"
+                />
+                <input value={month} className="date-add3" readOnly />
+                <input value={year} className="date-add4" readOnly />
+              </div>
             </div>
-            <div className=' mt-2'>
-        
-                <label>Pincode</label> <br></br>
-                <input type='text' className='input_section2 '/>
-         
+            <div className=" mt-2">
+              <label>Pincode</label> <br></br>
+              <input
+                type="text"
+                className="input_section2 "
+                onChange={(e) => setPincode(e.target.value)}
+              />
             </div>
-            <div className=''>
-                <label>Primary Skill</label> <br></br>
-                <div class="dropdown_section2 ">
-                    <select className="form-select h-100" id="inputGroupSelect01">
-                    <option></option>
-                    <option value="1">opt1</option>
-                    <option value="2">opt2</option>
-                    <option value="3">opt3</option>
-                    </select>
-                </div>
+            <div className="">
+              <label>Primary Skill</label> <br></br>
+              <div className="dropdown_section2 ">
+                <select
+                  className="form-select h-100"
+                  id="inputGroupSelect01"
+                  onChange={(e) => setPrimarySkill(e.target.value)}
+                >
+                  <option></option>
+                  <option value="1">opt1</option>
+                  <option value="2">opt2</option>
+                  <option value="3">opt3</option>
+                </select>
+              </div>
             </div>
             <div>
-                <label>Language</label> <br></br>
-                <div class=" dropdown_section2">
-                    <select className="form-select h-100" id="inputGroupSelect01">
-                    <option></option>
-                    <option value="1">opt1</option>
-                    <option value="2">opt2</option>
-                    <option value="3">opt3</option>
-                    </select>
-                </div>
+              <label>Language</label> <br></br>
+              <div className=" dropdown_section2">
+                <select
+                  className="form-select h-100"
+                  id="inputGroupSelect01"
+                  onChange={(e) => setLanguage(e.target.value)}
+                >
+                  <option></option>
+                  <option value="1">opt1</option>
+                  <option value="2">opt2</option>
+                  <option value="3">opt3</option>
+                </select>
+              </div>
             </div>
             <div>
-                <label>How many hours you can contribute daily?</label> <br></br>
-                <input type='text' className='input_section2'/> 
+              <label>How many hours you can contribute daily?</label> <br></br>
+              <input
+                type="text"
+                className="input_section2"
+                onChange={(e) => setContriHours(e.target.value)}
+              />
             </div>
-            <div className=''>
-                <label>Are you working on any other online platform?</label> <br></br>
-                <input type='radio' value={"Yes"} checked={selectedValue === 'Yes'} className='me-1' onChange={handleSelectionChange}/> <label className='me-3'>Yes</label>
-                <input type='radio' value={"No"} checked={selectedValue === 'No'} className='me-1' onChange={handleSelectionChange}/>  <label>No</label>
+            <div>
+              <label>Are you working on any other online platform?</label>
+              <br></br>
+              <input
+                type="radio"
+                value={"Yes"}
+                checked={workingOnOtherPlatforms === "Yes"}
+                className="me-1"
+                onChange={handleSelectionChange}
+              />
+              <label className="me-3">Yes</label>
+              <input
+                type="radio"
+                value={"No"}
+                checked={workingOnOtherPlatforms === "No"}
+                className="me-1"
+                onChange={handleSelectionChange}
+              />
+              <label>No</label>
             </div>
-        </div>     
-    </div>
-    <div className='btn-section'>
-
-    <button className='nextBt' onClick={handleNavToOtherDetails}>Next</button>
-    </div>
-</section>
-    </>)
-}
+          </div>
+        </div>
+        <div className="btn-section">
+          <button className="nextBt" onClick={sendDataToBackend}>
+            Next
+          </button>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default SkillDetail;
